2e86eb394b2be234a14e5fb53d389e252232e614
Kalyani Singh <kalyani.singh@myob.com>
2018-05-07 14:59:32 +1000

    pass simple Get

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,25 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        ServerSocket server = null;
-        try {
-            server = new ServerSocket(5000);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Listening for connection on port 5000 ....");
-        Socket socket = null;
+    try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            while (true) {
+                try (Socket connection = serverSocket.accept()) {
+                    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-        while (true) {
-            try {
-                Date today = new Date();
-                String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
-                socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
-
-            }
-            finally{
-                socket.close();
+                    while(inFromServer.ready()){
+                        inFromServer.read();
+                    }
+                    Date today = new Date();
+                    String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                    connection.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+                }
             }
         }
     }
